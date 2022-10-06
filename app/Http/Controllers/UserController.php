a98f397f597ce12436d74e9fe936a28e501fabd1
Hassan Sultan <h.hsultan277@gmail.com>
2022-10-06 18:24:13 +0500

    hassan

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -60,7 +60,7 @@ class UserController extends Controller
     }
     public function loggedInCreate(Request $request)
     {
-        dd($request->all());
+        // dd($request->all());
         try
         {
             $logged = new LoggedIn();
