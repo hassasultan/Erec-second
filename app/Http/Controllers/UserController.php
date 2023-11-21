6b632cc1dad45e7d5d88607aaf9c601c859c7111
Hassan Sultan <h.hsultan277@gmail.com>
2023-11-21 15:23:31 +0500

    pushed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,35 +25,43 @@ class UserController extends Controller
         // dd(env('APP_URL'));
         try
         {
-            $image = NULL;
-            $slug  = Str::slug($request->name.$request->lname);
-            $letter = str_split($request->lname);
-            if($request->hasFile('photo'))
+            $check = User::where('email',$request->email)->first();
+            if($check == null)
             {
-                $image  = $this->UserImage($request->photo);
-            }
-            $data = [
-                'u_type' => 3,
-                'f_name' => $request->name,
-                'm_name'=> $request->name,
-                'l_name'=> $request->lname,
-                'u_name'=> $slug,
-                'pass'   => "{X-PBKDF2}HMACSHA2+256:AAAD6A:zQMwAA==:djJr/w3qlfgGIEsWO5FM95iQSFepf398PkUIvNuAE9I=",
-                'org_id'=> 32,
-                'letter'=> $letter[0],
-                'institute_id'=> 1,
-                'email'=> $request->email,
-                'photo'=> $image,
-            ];
-            // dd($data);
-            $user = User::create($data);
-            if($user)
-            {
-                return  $user;
+                $image = NULL;
+                $slug  = Str::slug($request->name.$request->lname);
+                $letter = str_split($request->lname);
+                if($request->hasFile('photo'))
+                {
+                    $image  = $this->UserImage($request->photo);
+                }
+                $data = [
+                    'u_type' => 3,
+                    'f_name' => $request->name,
+                    'm_name'=> $request->name,
+                    'l_name'=> $request->lname,
+                    'u_name'=> $slug,
+                    'pass'   => "{X-PBKDF2}HMACSHA2+256:AAAD6A:zQMwAA==:djJr/w3qlfgGIEsWO5FM95iQSFepf398PkUIvNuAE9I=",
+                    'org_id'=> 32,
+                    'letter'=> $letter[0],
+                    'institute_id'=> 1,
+                    'email'=> $request->email,
+                    'photo'=> $image,
+                ];
+                // dd($data);
+                $user = User::create($data);
+                if($user)
+                {
+                    return  $user;
+                }
+                else
+                {
+                    return response()->json(['error'=>"Something Went Wrong..."]);
+                }
             }
             else
             {
-                return response()->json(['error'=>"Something Went Wrong..."]);
+                return  $check;
             }
 
         }
